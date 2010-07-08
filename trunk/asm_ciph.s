a51adefa4c7e961de85434bbf62941abbe449af4
gras.emmanuel@gmail.com <gras.emmanuel@gmail.com@6424431e-1536-bf6a-65f3-00b9f863367f>
2010-07-08 10:49:32 +0000

    Added CIPH instruction, and the companion example asm_ciph.s. Updated Global example asm_py.s

---
+++ trunk/asm_ciph.s
@@ -0,0 +1,6 @@
+CIPH this string is ciphered!! f*ck it# +-/
+SETA 0
+UNC
+SETA 0
+GETS
+PUTT
--- trunk/asm_py.s
+++ trunk/asm_py.s
@@ -9,3 +9,9 @@ PRINT you just entered:
 SETA 1
 GETS
 PUTT
+CIPH this string is ciphered!! f*ck it# +-/
+SETA 3
+UNC
+SETA 3
+GETS
+PUTT
